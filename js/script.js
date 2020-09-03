03def16e477133ed6a9ac3ec5c2c3b6b5d73e3ff
Remi <not.iremy@gmail.com>
2020-09-03 23:55:35 +0200

    add cards and js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-console.log('coucou')
-var string = "my string"; // What i want to pass to php
-var stringb = "my stringb"; // What i want to pass to php
+//console.log('coucou')
 
-$.ajax({
+
+/* $.ajax({
     type: 'post', // the method (could be GET btw)
     url: 'model/data.json', // The file where my php code is
     success: function(data) { // in case of success get the output, i named data
@@ -30,5 +29,98 @@ $.ajax({
         
         
     }
+}); */
+let save = document.getElementById('save')
+save.style.display = "none"
+let success = document.getElementById('success')
+success.style.display = "none"
+
+let state = true
+
+
+let card = document.getElementsByClassName('card')
+
+for(let t = 0; t < card.length; t++){
+    card[t].addEventListener("click", () => {
+        state = !state
+        console.log(state)
+        if(state == false){
+            //console.log(state)
+            card[t].style.background = "yellow" 
+        }
+        else{       
+            card[t].style.background = "white"
+            //card[t].classList.remove('click');
+        }
+    })
+}
+
+
+
+
+var string = "my string"; // What i want to pass to php
+var stringb = "my stringb"; // What i want to pass to php
+
+
+$.ajax({
+    method: "post",
+    url: "../model/data.json",
+    dataType: "json",
+
+    success: function() {
+        console.log("Ajax success");
+    },
+    error: function() {
+        alert("There was an error. Try again please!");
+    },
+})
+//.done est lié a jquery ref la doc
+.done(function(data) {
+    //console.log(data);
+        for(let j = 0; j < card.length; j++){
+            console.log(card);
+
+                console.log(data)
+                
+                card[j].innerHTML = '<img src="' + data[j].urlavatar + '">' + data[j].nom 
+                + '<br>' + data[j].pseudo + '<br>' + data[j].xp;
+                
+            card[j].addEventListener("click", () => {
+
+                
+                card[j].classList.add('click')
+                let clicked = document.getElementsByClassName('click')
+                
+                if(clicked.length == 2){
+                    console.log("coucoui")
+                    save.style.display = "block"
+                    save.addEventListener("click", () => {
+
+                        $.ajax({
+                            type: 'post', // the method (could be GET btw)
+                            url: '../view/test.php', // The file where my php code is
+                            data: {
+                                'test': string,
+                                'testb' : stringb,
+                                'player': data[0].player,
+                                'playerb': data[1].player // all variables i want to pass. In this case, only one.
+                        
+                            },
+                            success: function(data) { // in case of success get the output, i named data
+                                //alert(data + ' Rencontre saved'); // do something with the output, like an alert
+                                success.style.display = "block"
+                                save.style.display = "none"
+                            },
+                            error: function(data){
+                                console.log(data)
+                            }
+                        }); 
+                    })
+                }
+
+            });
+
+            
+        }
 });
 
--- view/espaceMembre.php
+++ view/espaceMembre.php
@@ -14,11 +14,45 @@ session_start();
     }
 ?>
 
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-4 text-center">
+      <div class="card" style="width: 18rem;">
+       
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 text-center">
+      <div class="card" style="width: 18rem;">
+      
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 text-center">
+      <div class="card" style="width: 18rem;">
+       
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+      <button class="btn btn-info justify-content-center" id="save">Save it ! </button>
+      <div class="alert alert-success" id="success" role="alert">
+        Success !
+      </div>
+  </div>
+</div>
+
+
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="js/script.js"></script>   
+<script src="../js/script.js"></script>   
 </body>
 </html>
